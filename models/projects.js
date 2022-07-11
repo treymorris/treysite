4e8318f0f82e9b3eec4b1dae2231d5ed5507be8d
Trey Morris <treymorrisit@hotmail.com>
2022-07-11 09:33:49 -0400

    Add published status to projects

--- models/projects.js
+++ models/projects.js
@@ -6,7 +6,8 @@ var ProjectSchema = new Schema({
     name: { type: String },
     url: { type: String },
     src: { type: String },
-    description: { type: String }
+    description: { type: String },
+    published: { type: Boolean }
 });
 
 module.exports = mongoose.model('Project', ProjectSchema)
